89edbff949651ead0e633aa9c93aee2c9a9e0ea6
gpnasd289 <gpnasd289@gmail.com>
2021-08-30 21:17:08 +0700

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <div>
+        <?php
+            echo "<h1>Xin chào, Hoàng Hiệp</h1>";
+        ?>
+    </div>
+</body>
+</html>
